00e8f3be689f48574a29e22064c358a16d0f71f2
Ali Muhammad <alim17347@gmail.com>
2024-06-14 13:37:36 +0700

    Initial backend project fixed

--- wms-frontend/src/components/AdminComponent/index.js
+++
@@ -1,6 +0,0 @@
-import Navbar from "./Navbar";
-import Logobar from "./Logobar";
-import Sidebar from "./Sidebar";
-import Contents from "./Contents";
-
-export { Navbar, Logobar, Sidebar, Contents };
--- wms-frontend/src/components/PemilikComponent/ContentPages/index.js
+++
@@ -1,5 +0,0 @@
-import HalamanUtama from "./HalamanUtama";
-import ManajemenBahan from "./ManajemenBahan";
-import LaporanBahan from "./LaporanBahan";
-
-export { HalamanUtama, ManajemenBahan, LaporanBahan };
